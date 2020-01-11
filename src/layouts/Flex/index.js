760fbd71553934c7a4915b1e1c265953f0318db5
Conner Wells <conner.c.wells@gmail.com>
2020-01-11 15:34:17 -0800

    adds new layout Flex

--- src/layouts/Flex/index.js
+++ src/layouts/Flex/index.js
@@ -1,1 +1,48 @@
-index.js
+// flex.js
+export const FlexChild = (props) => {
+  const {
+    variant,
+    className,
+    children,
+    ...rest
+  } = props;
+
+  const classes = [
+    'FlexChild',
+    variant && `FlexChild--${variant}`,
+    className
+  ];
+
+  const classStack = classes.join(' ');
+
+  return (
+    <div className={classStack} {...rest}>
+      {children}
+    </div>
+  );
+};
+
+export const Flex = (props) => {
+  const {
+    variant,
+    className,
+    children,
+    ...rest
+  } = props;
+
+  const classes = [
+    'Flex',
+    variant && `Flex--${variant}`,
+    className
+  ];
+
+  const classStack = classes.join(' ');
+
+  return (
+    <div className={classStack} {...rest}>
+      {children}
+    </div>
+  );
+};
+
+export default Flex;
